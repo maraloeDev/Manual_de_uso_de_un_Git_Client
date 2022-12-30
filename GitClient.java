76278822b0bc2a6a8593bbf51c112d468ceb3727
maraloeDev <95626566+maraloeDev@users.noreply.github.com>
2022-12-30 19:52:23 +0100

    Update

---
+++ GitClient.java
@@ -0,0 +1,44 @@
+package appgitclient;
+
+public class GitClient {
+
+    private String cliente;
+    private String version;
+    private String sistemaOperativo;
+    private String licencia;
+
+    public GitClient() {
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getSistemaOperativo() {
+        return sistemaOperativo;
+    }
+
+    public void setSistemaOperativo(String sistemaOperativo) {
+        this.sistemaOperativo = sistemaOperativo;
+    }
+
+    public String getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(String licencia) {
+        this.licencia = licencia;
+    }
+}
